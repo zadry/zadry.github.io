c09d96c1302c1f7cb827deba799224f18734338d
zadry <armoniadelaberintos@gmail.com>
2017-06-21 23:08:06 -0500

    Update conn.js

--- conn.js
+++ conn.js
@@ -2,7 +2,7 @@
 //document.getElementById('boton').addEventListener('onclick', read);
 
 function read(){
-  //alert("algo");
+  alert("algo");
   navigator.bluetooth.requestDevice({
   filters: [{
     services: [0x1234, 0x12345678, '99999999-0000-1000-8000-00805f9b34fb']
